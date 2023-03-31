28bda02315bcd897458384ad6645b367b46b7457
clipKoala <s.jozwiak@htlstp.at>
2023-03-31 16:56:57 +0200

    Level_2 - not finish

--- src/MainStart.java
+++ src/MainStart.java
@@ -1,4 +1,5 @@
 import level_1.Level_1;
+import level_2.Level_2;
 
 import java.nio.file.Path;
 import java.util.stream.Stream;
@@ -6,24 +7,28 @@ import java.util.stream.Stream;
 public class MainStart {
     public static void main(String[] args) {
 
-        Stream<String> input = new FileOperations().readFile(Path.of("resources/level_1/level1_1.in"));
-        String result = new Level_1().solve(input);
-        new FileOperations().writeFile(Path.of("src/level_1/level1_1.out"), result);
+//        Stream<String> input = new FileOperations().readFile(Path.of("resources/level_2/level2_example.in"));
+//        String result = new Level_2().solve(input);
+//        new FileOperations().writeFile(Path.of("src/level_2/level2_example.out"), result);
 
-        input = new FileOperations().readFile(Path.of("resources/level_1/level1_2.in"));
-        result = new Level_1().solve(input);
-        new FileOperations().writeFile(Path.of("src/level_1/level1_2.out"), result);
-
-        input = new FileOperations().readFile(Path.of("resources/level_1/level1_3.in"));
-        result = new Level_1().solve(input);
-        new FileOperations().writeFile(Path.of("src/level_1/level1_3.out"), result);
-
-        input = new FileOperations().readFile(Path.of("resources/level_1/level1_4.in"));
-        result = new Level_1().solve(input);
-        new FileOperations().writeFile(Path.of("src/level_1/level1_4.out"), result);
-
-        input = new FileOperations().readFile(Path.of("resources/level_1/level1_5.in"));
-        result = new Level_1().solve(input);
-        new FileOperations().writeFile(Path.of("src/level_1/level1_5.out"), result);
+        Stream<String> input = new FileOperations().readFile(Path.of("resources/level_2/level2_1.in"));
+        String result = new Level_2().solve(input);
+        new FileOperations().writeFile(Path.of("src/level_2/level2_1.out"), result);
+//
+//        input = new FileOperations().readFile(Path.of("resources/level_2/level2_2.in"));
+//        result = new Level_2().solve(input);
+//        new FileOperations().writeFile(Path.of("src/level_2/level2_2.out"), result);
+//
+//        input = new FileOperations().readFile(Path.of("resources/level_2/level2_3.in"));
+//        result = new Level_2().solve(input);
+//        new FileOperations().writeFile(Path.of("src/level_2/level2_3.out"), result);
+//
+//        input = new FileOperations().readFile(Path.of("resources/level_2/level2_4.in"));
+//        result = new Level_2().solve(input);
+//        new FileOperations().writeFile(Path.of("src/level_2/level2_4.out"), result);
+//
+//        input = new FileOperations().readFile(Path.of("resources/level_2/level2_5.in"));
+//        result = new Level_2().solve(input);
+//        new FileOperations().writeFile(Path.of("src/level_2/level2_5.out"), result);
     }
 }
--- src/level_2/Level_2.java
+++ src/level_2/Level_2.java
@@ -1,4 +1,66 @@
 package level_2;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Level_2 {
+
+    public String solve(Stream<String> input) {
+
+        var inputList = input.toList();
+
+        var wins = "";
+        while (true) {
+            if (inputList.get(1).length() == 2) {
+                break;
+            }
+
+            wins = getRound(inputList);
+            inputList = Arrays.stream(wins.split("\n")).toList();
+
+        }
+
+        return wins;
+    }
+
+    private String getRound(List<String> inputList) {
+        String wins = "";
+        for (int i = 0; i < inputList.size(); i++) {
+            if (inputList.get(i).length() % 2 != 0) {
+                continue;
+            }
+
+            for (int j = 0; j < inputList.get(i).length(); j += 2) {
+                wins += getWinners(inputList.get(i).substring(j, j + 2));
+            }
+
+            wins += "\n";
+        }
+        return wins;
+    }
+
+    private String getWinners(String fight) {
+        var wins = "";
+        if (fight.contains("P") && fight.contains("S")) {
+            wins += "S";
+        }
+        if (fight.contains("P") && fight.contains("R")) {
+            wins += "P";
+        }
+        if (fight.contains("R") && fight.contains("S")) {
+            wins += "R";
+        }
+        if (fight.charAt(0) == 'P' && fight.charAt(1) == 'P') {
+            wins += "P";
+        }
+        if (fight.charAt(0) == 'R' && fight.charAt(1) == 'R') {
+            wins += "R";
+        }
+        if (fight.charAt(0) == 'S' && fight.charAt(1) == 'S') {
+            wins += "S";
+        }
+
+        return wins;
+    }
 }
