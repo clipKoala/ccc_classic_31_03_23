c70ef9469428f10247b21ba3a826fe700ef35d4f
clipKoala <s.jozwiak@htlstp.at>
2023-03-31 13:59:38 +0200

    Level_1

--- src/level_1/Level_1.java
+++ src/level_1/Level_1.java
@@ -1,4 +1,12 @@
 package level_1;
 
+import java.util.stream.Stream;
+
 public class Level_1 {
+
+    public String solve(Stream<String> input) {
+        return input
+                .filter((mark) -> mark.equals("x"))
+                .count() + "";
+    }
 }
